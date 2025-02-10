adcbef4a2c20c0357e7a35f8b53e1dd5d4f62308
Tiuxi <thigal.pierre@gmail.com>
2025-02-10 18:49:32 +0100

    find executable (work only once)

--- src/constants.h
+++ src/constants.h
@@ -5,11 +5,19 @@
 #define ROOTSH_MAX_ERROR_LENGTH 256
 #define PATHVAR "PATH"
 
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
 // Syscall checks
 #include <stdio.h>
 
 #define ASSERT(op)           \
-    if(op == -1) {          \
+    if((op) == 0) {          \
         fprintf(stderr, "Error: line %d, file \"%s\"\n", __LINE__, __FILE__);   \
         fflush(stderr);     \
         exit(EXIT_FAILURE); \
--- src/execCommand.c
+++ src/execCommand.c
@@ -7,7 +7,7 @@ List getEnvironementsDir()
 {
     char *pathstr = getenv(PATHVAR);
     if (pathstr == NULL)
-        ASSERT(-1);
+        ASSERT(FALSE);
 
     int lenPath = strlen(pathstr);
 
@@ -34,32 +34,105 @@ List getEnvironementsDir()
     return paths;
 }
 
+char* get_single_command(List command, List paths) {
+    char* executablePath = (char*)malloc(sizeof(char) * PATH_MAX);
+    int found = 0;
+
+    // Check redirection
+    Error errorRedirect = rootshError_new_error();
+    if (rootshInput_checkRedirect(command, errorRedirect) == -1) {
+        rootshError_print_error(errorRedirect);
+    }
+    rootshError_destroy_error(errorRedirect);
+
+    // if is a file, go to that location
+    if (ISFILE(command)) {
+        
+    }
+
+    // if not a file, check for executables in "PATH"
+    else {
+        DIR* dir;
+
+        // open every "PATH" directories
+        List currentDir = paths;
+        while (currentDir != NULL && !found) {
+            // open dir
+            dir = opendir(currentDir->v);
+            if (dir==NULL) {
+                // if dir not found, skip to next dir
+                if (errno == 2) {
+                    currentDir = currentDir->next;
+                    continue;
+                }
+
+                // uknown error, stop
+                else ASSERT(FALSE);
+            }
+
+            struct dirent* file = readdir(dir);
+            //struct stat st;
+
+            // check for every file in directory
+            while (file != NULL && !found) {
+
+                // check file name
+                if (strncmp(file->d_name, command->v, NAME_MAX) == 0){
+                    found = 1;
+                    snprintf(executablePath, PATH_MAX, "%s/%s", (char*)currentDir->v, file->d_name);
+                }
+
+                // swap to next file in directory
+                errno = 0;
+                file = readdir(dir);
+            }
+
+            ASSERT(closedir(dir) != -1);
+
+            currentDir = currentDir->next;
+        }
+    }
+
+    if (found==0) {
+        free(executablePath);
+        return NULL;
+    }
+    
+    return executablePath;
+}
+
 void rootshExec_execute_command(char* command) {
-    Error error = rootshError_new_error();
     List paths = getEnvironementsDir();
 
     // parse the command
     List commands = rootshInput_splitInput(command);
 
-    // Check redirection
     for (List command=commands; command!=NULL; command=command->next) {
         List entry = command->v;
 
-        rootshList_printListString(entry);
-        //rootshList_printListString(entry);
-        if (rootshInput_checkRedirect(entry, error) == -1) {
-            rootshError_print_error(error);
+        // Check redirection
+
+        // Check file
+
+        // Check "PATH" executables
+        char* executable = get_single_command(entry, paths);
+
+        if (executable==NULL) {
+            Error err = rootshError_new_error();
+            rootshError_set_error_message(err, "Command not found");
+            rootshError_print_error(err);
+            rootshError_destroy_error(err);
+        }else {
+            printf("%s\n", executable);
+            free(executable);
         }
+
+        // execute command
+
     }
-
-    // Check files
-
-    // Execute command
-    rootshList_printListString(paths);
 
     // free everything
     rootshList_destroy2DListAll(commands);
-    rootshError_destroy_error(error);
 
     return;
 }
