9641d82db82ffbaa5bc632651822a4217b2dcd4f
tgallone <thigal.pierre@gmail.com>
2026-06-22 23:37:48 +0200

    Fixed command size (testing only)

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main (int argc, char** argv) {
     (void) argv;
 
     int running = 1;
-    char buffer[PLUSH_BASE_COMMAND_LENGTH];
+    char buffer[500];
     int index = 0;
 
     // history
@@ -30,7 +30,7 @@ int main (int argc, char** argv) {
             // if command is empty, skip
             if (index!=0) {
                 buffer[index] = '\0';
-                if (strncmp(buffer, "exit", PLUSH_BASE_COMMAND_LENGTH) == 0)
+                if (strncmp(buffer, "exit", 500) == 0)
                     running = FALSE;
                 else {
                     plushHistory_add_command(buffer);
